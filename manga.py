b233d127c4dc2e78301fc4ed7ea527c8c8cc1874
Tyler Stokes <tystokes@umich.edu>
2019-10-24 18:40:22 -0400

    Add new mangahere algorithm.

--- manga.py
+++ manga.py
@@ -4,6 +4,7 @@ from os import makedirs
 from os.path import dirname, exists, isfile, getsize
 from sys import getfilesystemencoding
 from urllib.request import build_opener
+from urllib.error import HTTPError
 from threading import Thread, Lock, Semaphore
 from queue import Queue
 from json import load, dump
@@ -16,13 +17,18 @@ encoding = getfilesystemencoding()
 filesystemLock = Lock()
 
 # max number of simultaneous web requests
-max_workers = Semaphore(value=5)
+max_workers = Semaphore(value=4)
 
 def ensure_dir(f):
     with filesystemLock:
         d = dirname(f)
         if not exists(d):
             makedirs(d)
+
+CHARS = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+def decode(cypher, strs):
+    return ''.join(strs[CHARS.index(c)] if c in CHARS and strs[CHARS.index(c)] else c for c in cypher)
 
 Option = namedtuple('Option', ['text', 'value'])
 
@@ -45,7 +51,9 @@ def update(filename):
                 index_file[current_site].append(sub(r'[\'", ]', '', l.strip()))
     for k, v in index_file.items():
         for s in v:
-            series(k, s).start()
+            tmp = series(k, s)
+            tmp.start()
+            tmp.join()
 
 def comment(site, title):
     if not index_filename:
@@ -68,12 +76,12 @@ def comment(site, title):
     with open(index_filename, "w") as f:
         f.write(total)
 
-"""
-A JSONDict is a dictionary that automatically saves itself when modified.
-Used to automatically save changes to the cache/index.
-"""
 class JSONDict(MutableMapping):
-    
+    """
+    A JSONDict is a dictionary that automatically saves itself when modified.
+    Used to automatically save changes to the cache/index.
+    """
+
     def new(self):
         self.store = dict()
         with open(self.filename, 'w') as f:
@@ -113,17 +121,17 @@ class JSONDict(MutableMapping):
 # Stores series that are no longer being serialized and are completely downloaded.
 main_index = JSONDict('./index.json')
 
-""" 
-Repeatedly tries to open the specified URL.
-After 10 failed attempts it will raise an exception.
-"""
-def repeat_urlopen(my_str, url=None, attempts=3):
+def repeat_urlopen(my_str, url=None, attempts=5):
+    """
+    Repeatedly tries to open the specified URL.
+    After 5 failed attempts it will raise an exception.
+    """
     with max_workers:
         for i in range(attempts):
             try:
                 # Build up a user-agent header to spoof as a normal browser
                 opener = build_opener()
-                opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+                opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36')]
                 if url:
                     opener.addheaders = [('Referer', url)]
                 tmp = opener.open(my_str)
@@ -133,9 +141,15 @@ def repeat_urlopen(my_str, url=None, attempts=3):
                     if tmp.info().get('Content-Encoding') == 'gzip':
                         return decompress(tmp.read())
                     return tmp.read()
+            except HTTPError as e:
+                if e.code == 401:
+                    print(my_str, e, 'Skipping.')
+                    break
+                print(my_str, e)
+                sleep(10)
             except Exception as e:
-                print(e)
-                sleep(1)
+                print(my_str, e)
+                sleep(10)
         raise Exception('Unable to resolve URL (%s) after %d attempts.' % (my_str, attempts))
 
 class series(Thread):
@@ -171,6 +185,8 @@ class series(Thread):
         Thread.__init__(self)
         self.sort = sort
         self.site = site
+        tmp = site[site.index('//')+2:]
+        self.site_root = tmp[:tmp.index('/')]
         self.title = title
         self.digits = digits
         self.reindex = reindex
@@ -178,18 +194,25 @@ class series(Thread):
         self.workers = []
         for i in range(workers):
             t = Thread(target=self.work_page)
-            t.setDaemon(True)
+            t.daemon = True
             self.workers.append(t)
             t.start()
         self.index = JSONDict('./%s/index.json' % self.title, self.reindex)
 
+    def clean_link(self, link):
+        if link.startswith("//"):
+            link = "http:" + link
+        if not link.startswith("http"):
+            link = "http://" + self.site_root + link
+        return link
+
     def get_chapters(self):
         try:
             my_str = '%s/%s/' % (self.site, self.title)
-            soup = BeautifulSoup(repeat_urlopen(my_str))
+            soup = BeautifulSoup(repeat_urlopen(my_str), 'html.parser')
         except:
             my_str = '%s/%s' % (self.site, self.title)
-            soup = BeautifulSoup(repeat_urlopen(my_str))
+            soup = BeautifulSoup(repeat_urlopen(my_str), 'html.parser')
         links = soup.find_all('a')
         all_chapters = set()
         self.completed = False
@@ -199,93 +222,85 @@ class series(Thread):
                 status_label = li.find("label")
                 if not status_label:
                     continue
-                if 'Status' in status_label.getText().encode('ascii', 'ignore').decode('ascii') and 'Completed' in li.getText().encode('ascii', 'ignore').decode('ascii'):
+                if 'Status' in status_label.getText() and 'Completed' in li.getText():
                     self.completed = True
                     break
         except Exception as e:
             pass
         for l in links:
             try:
-                if search(my_str + r'.*?c?([0-9]+(\.[0-9]+)?)/([0-9]+(\.html)?)?\Z', l['href']):
-                    all_chapters.add(l['href'])
-                elif search(self.title + r'.*?c?([0-9]+(\.[0-9]+)?)/([0-9]+(\.html)?)?\Z', l['href']):
-                    # TODO: work on supporting mangapanda
-                    all_chapters.add(self.site + l['href'])
+                l = self.clean_link(l['href'])
+                if '.' in l and self.title in l and search(r'.*?c?([0-9]+(\.[0-9]+)?)/([0-9]+(\.html)?)?$', l):
+                    all_chapters.add(l)
             except:
                 continue
         return all_chapters
 
     def work_chapter(self, my_str):
-        try:
-            search_str = '%s/%s' % (self.site, self.title)
-            prefix = search(r'(.*?c?[0-9]+(\.[0-9]+)?/)([0-9]+(\.html)?)?\Z', my_str).group(1)
-            ch_search = search(search_str + r'.*?c?([0-9]+(\.([0-9]+))?)/([0-9]+(\.html)?)?\Z', my_str)
-            c = ch_search.group(1)
-            if ch_search.group(2):
-                c = int(ch_search.group(1)[:-len(ch_search.group(2))])
-            else:
-                c = int(c)
-            self.padded_chapter = '0' * (self.digits - len(str(c))) + str(c)
-            self.extra_chapter_add = 0
-            self.extra_chapter = 0
-            if ch_search.group(3):
-                self.extra_chapter = int(ch_search.group(3))
-                self.extra_chapter_add = 10**(self.digits-1) * self.extra_chapter
-            if str(c) + '.' + str(self.extra_chapter) in self.index:
-                return
-            # what about case when page is 404 or something?
-            soup = BeautifulSoup(repeat_urlopen(my_str))
-            opts = soup.find_all('option')
-            all_opts = set()
-            opts_list = list()
-            for o in opts:
-                all_opts.add(Option(o.text, o.get("value")))
-            for o in all_opts:
-                s = search(search_str + r'.*?c?[0-9]+(\.([0-9]+))?/(([0-9]+)(\.html)?)?\Z', o.value)
-                if not s:
-                    if o.text and search(r'#?[0-9]+( / .*)?\Z', o.text) and search(r'\A[0-9]+\Z', o.value):
-                        opts_list.append([prefix + o.value, o.value])
-                    elif search(r'\A[0-9]+\Z', o.text) and search(r'\A/', o.value):
-                        opts_list.append([self.site + o.value, o.text])
-                elif s.group(3):
-                    opts_list.append([o.value, s.group(4)])
-                else:
-                    opts_list.append([o.value, 1])
-            for o in opts_list:
-                self.q.put(o)
-            self.q.join()
-            if len(opts_list) > 0:
-                self.index[str(c) + '.' + str(self.extra_chapter)] = True
-        except:
-            raise
+        search_str = self.title
+        prefix = search(r'(.*?c?[0-9]+(\.[0-9]+)?/)([0-9]+(\.html)?)?$', my_str).group(1)
+        ch_search = search(search_str + r'.*?c?([0-9]+(\.([0-9]+))?)/([0-9]+(\.html)?)?$', my_str)
+        if ch_search is None:
+            print('Error:', search_str, my_str)
+            return
+        c = ch_search.group(1)
+        if ch_search.group(2):
+            c = int(ch_search.group(1)[:-len(ch_search.group(2))])
+        else:
+            c = int(c)
+        self.padded_chapter = '0' * (self.digits - len(str(c))) + str(c)
+        self.extra_chapter_add = 0
+        self.extra_chapter = 0
+        if ch_search.group(3):
+            self.extra_chapter = int(ch_search.group(3))
+            self.extra_chapter_add = 10**(self.digits-1) * self.extra_chapter
+        index_key = str(c) + '.' + str(self.extra_chapter)
+        if index_key in self.index and self.index[index_key]:
+            return
+        # TODO: What about case when page is 404 or something?
+        soup = BeautifulSoup(repeat_urlopen(my_str), 'html.parser')
+        for script_tag in soup.find_all('script'):
+            contents = script_tag.decode_contents()
+            if 'chapterid' in contents:
+                comicid = search(r'var comicid *= *(\d+);', contents).group(1)
+                chapterid = search(r'var chapterid *= *(\d+);', contents).group(1)
+                imagecount = int(search(r'var imagecount *= *(\d+);', contents).group(1))
+                print('comicid =', comicid,
+                      '; chapterid =', chapterid,
+                      '; imagecount =', imagecount)
+        dm5_key = soup.find("input", {"id": "dm5_key"})['value']
+        self.failure_list = list()
+        for i in range(1, imagecount+1):
+            fun = my_str.split('1.html')[0] + 'chapterfun.ashx?cid=%s&page=%s&key=%s' % (chapterid, i, dm5_key)
+            fun_data = repeat_urlopen(fun, my_str).decode('UTF-8')
+            print(fun_data)
+            strings = search(r".+'(.+)'\.split", fun_data).group(1).split('|')
+            part1 = search(r'{\d .=\"(.+?)\"', fun_data).group(1)
+            part2 = search(r';\d .=\[\"(.+?)\"', fun_data).group(1)
+            image_url = 'http:' + decode(part1, strings) + decode(part2, strings)
+            self.q.put([image_url, i, my_str])
+        self.q.join()
+        if imagecount > 0 and len(self.failure_list) == 0:
+            self.index[index_key] = True
 
     def work_page(self):
         while True:
             o = self.q.get()
+            image_url = o[0]
             p = int(o[1]) + self.extra_chapter_add
+            referer_url = o[2]
+            print(o)
             padded_page = '0' * (self.digits - len(str(p))) + str(p)
             filename = './%s/%s.%s.jpg' % (self.title, self.padded_chapter, padded_page)
             ensure_dir(filename)
             if not isfile(filename) or getsize(filename) < 100:
                 print(self.title, self.padded_chapter, padded_page)
-                page = repeat_urlopen(o[0])
-                if not page:
-                    continue
-                soup = BeautifulSoup(page)
-                ids = ('image', 'picture')
-                for i in ids:
-                    image = soup.find('img',{'id':i})
-                    if image:
-                        break
-                if not image:
-                    continue
-                image_url = image['src']
-                if 'http://' not in image_url:
-                     image_url = self.site + image_url
                 try:
-                    with open(filename,'wb') as f:
-                        f.write(repeat_urlopen(image_url, o[0]))
+                    bytes_to_write = repeat_urlopen(image_url, referer_url)
+                    with open(filename, 'wb') as f:
+                        f.write(bytes_to_write)
                 except:
+                    self.failure_list.append(o[1])
                     print('Unable to download %s.' % filename)
             self.q.task_done()
 
@@ -298,13 +313,7 @@ class series(Thread):
         print('**************** starting %s ****************' % self.title)
         chapters = self.get_chapters()
         for c in self.sorted(chapters):
-            while True:
-                try:
-                    self.work_chapter(c)
-                    break
-                except Exception as e:
-                    print('Error', e)
-                    continue
+            self.work_chapter(c)
         if self.completed:
             with filesystemLock:
                 main_index[self.title] = True
